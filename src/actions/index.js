8582e6b30752e1d97fb10302745771a6046be645
pawel misiak <pawelmisiak@live.com>
2018-04-18 14:45:47 -0400

    Successfully got a response from a promise that is handled by the middleware in payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,22 @@
+//comes from new library that handles promises
+import axios from 'axios';
+
 const API_KEY = '42339cc4b1dd910d501d76332e4ea5dc';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// in case of any typos in string it is good practice
+// to keep it in a variable
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  //this will return promise
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    paylod: request,
+  };
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+// We take out export default from line below
+// Once we specify export default connect on the bottom of the file
+class SearchBar extends Component {
 
   constructor(props) {
     super(props);
@@ -10,10 +15,11 @@ export default class SearchBar extends Component {
     // this is a different approach from using fat arrow f:
     // in Change={this.onInputChange}
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
-    console.log(event.target.value);
+    // console.log(event.target.value);
 
     // this allows the search_bar to be filled with value
     this.setState({ term: event.target.value });
@@ -23,6 +29,10 @@ export default class SearchBar extends Component {
     // prevents the browser to submit POST <form> automatically
     event.preventDefault();
     // here we need to fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+    // if there is an error 'Cannot read property 'props' of null', than
+    // in constructor you have to bind (reffer to constructor)
   }
 
   render() {
@@ -41,3 +51,11 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// passing null to make sure that the function always goes as
+// a second argument
+export default connect(null, mapDispatchToProps)(SearchBar);
